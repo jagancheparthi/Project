89f42707692c6f2fc54e6a7eb61730597b620f44
jagan gopi <jgncpr@gmail.com>
2023-12-14 16:01:17 +0530

    Second commit

--- src/main/java/com/te/hrms/constants/SkillConstants.java
+++ src/main/java/com/te/hrms/constants/SkillConstants.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Component;
 public class SkillConstants {
 	public final String MSG="TRUE INDICATES SUCCESSFUL && FALSE INSICATES FAILURE";
     public final String ticket="Ticket1";
+    public final String sav="Check";
 }
